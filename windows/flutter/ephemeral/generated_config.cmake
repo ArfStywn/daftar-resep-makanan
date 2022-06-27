3f50c0b5fa4c304ca996624df11f9555bcb02eba
Arif Setyawan <103981622+ArfStywn@users.noreply.github.com>
2022-06-27 15:51:54 +0700

    1

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\flutter_app\\resep_makanan" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "PROJECT_DIR=C:\\flutter_app\\resep_makanan"
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\flutter_app\\resep_makanan\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\flutter_app\\resep_makanan"
+  "FLUTTER_TARGET=C:\\flutter_app\\resep_makanan\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\flutter_app\\resep_makanan\\.dart_tool\\package_config.json"
+)
